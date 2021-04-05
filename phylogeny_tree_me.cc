fb0832c1d644cd2f367682aadd8293d60486bb61
Mojave <ak461717@ohio.edu>
2021-04-05 19:27:52 -0400

    charstring

--- phylogeny_tree_me.cc
+++ phylogeny_tree_me.cc
@@ -196,7 +196,7 @@ int main(int argc, char *argv[]) {
  }
 
 
-  cout<<endl<<"genomes size: 1  "<<genomes[0];
+  //cout<<endl<<"genomes size: 1  "<<genomes[0];
 
   //cout<<endl<<"char_string size: 2  "<<char_string[0];
 
