4fa6c388f9513fb8f22df6e495e6d5e06eea9654
Mojave <ak461717@ohio.edu>
2021-04-06 01:44:59 -0400

    charstring

--- phylogeny_tree_me.cc
+++ phylogeny_tree_me.cc
@@ -255,10 +255,11 @@ int main(int argc, char *argv[]) {
      string best_temp(char_best.begin(), char_best.end());
      best = best_temp;
 
-  // string new_tree_label = "("+genome_tree[max_i].first + "," + genome_tree[max_j].first +")";
-  // genome_tree.erase(genome_tree.begin()+max_i);
-  // genome_tree.erase(genome_tree.begin()+max_j-1); // max_i got deleted!
-  // genome_tree.push_back(make_pair(new_tree_label,best));
+  string new_tree_label = "("+genome_tree[max_i].first + "," + genome_tree[max_j].first +")";
+  genome_tree.erase(genome_tree.begin()+max_i);
+  genome_tree.erase(genome_tree.begin()+max_j-1); // max_i got deleted!
+  genome_tree.push_back(make_pair(new_tree_label,best));
+
 }
 
   // cout << "Phylogeny = " << endl;
