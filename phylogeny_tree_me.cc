4e59bb1c2b5647f944d043dc1395ae07571c5f14
Mojave <ak461717@ohio.edu>
2021-04-06 23:27:31 -0400

    ijmax

--- phylogeny_tree_me.cc
+++ phylogeny_tree_me.cc
@@ -276,9 +276,11 @@ int main(int argc, char *argv[]) {
 
      //cout<<endl<<"genome tree start "<<genome_tree.begin();
 
+     max_j = max_j - 1;
+
      string new_tree_label = "("+genome_tree[max_i].first + "," + genome_tree[max_j].first +")";
      genome_tree.erase(genome_tree.begin()+max_i);
-     //genome_tree.erase(genome_tree.begin()+max_j-1); // max_i got deleted!
+     genome_tree.erase(genome_tree.begin()+max_j); // max_i got deleted!
      // genome_tree.push_back(make_pair(new_tree_label,best));
 }
 
