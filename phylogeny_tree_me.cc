679e636a7aee0910a8c6261ffead5c3a8804817e
Mojave <ak461717@ohio.edu>
2021-04-07 13:43:34 -0400

    myid4debug

--- phylogeny_tree_me.cc
+++ phylogeny_tree_me.cc
@@ -222,7 +222,7 @@ int main(int argc, char *argv[]) {
   bool start = true;
   int loc_length, global_longest;
   if(myid != 0){
-    for(int k = myid; k < proc_pair.size(); k = k + num_procs){
+    for(int k = myid-1; k < proc_pair.size(); k = k + num_procs){
          //cout<<endl<<"K: "<<k;
       int i = proc_pair[k].first;
       int j = proc_pair[k].second;
